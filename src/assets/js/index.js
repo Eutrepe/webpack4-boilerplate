4c85a16c6f6acb8a1b16e4771ae4fd4f4eea2501
Maciej Bobrowski <mabobrowski@gmail.com>
2020-01-20 23:43:04 +0100

    clean index.htm and index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -8,27 +8,7 @@ import '../scss/style.scss';
     'use strict';
 
     $(
-        () => {
-            const button = document.querySelector('button');
-            const textParagraph = document.querySelector('p');
-            
-            button.addEventListener('click', () => {
-              const text = textParagraph.textContent;
-              const promise = new Promise((resolve, reject) => {});
-              console.log(promise);
-              if (navigator.clipboard) {
-                navigator.clipboard
-                  .writeText(text)
-                  .then(result => {
-                    console.log(result);
-                  })
-                  .catch(error => {
-                    console.log(error);
-                  });
-              } else {
-                alert('Feature not available, please copy manually!');
-              }
-            });
+        () => {    
            
         }
     )    
